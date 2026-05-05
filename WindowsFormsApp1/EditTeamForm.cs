2090c985a7f31461c8fcf54b1109284c8afe6d81
hygard <kirillkiselov0705@gmail.com>
2026-05-05 23:47:49 +0300

    v. 67
    tochno final04ka

---
+++ WindowsFormsApp1/EditTeamForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApp1
+{
+    public partial class EditTeamForm : Form
+    {
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database1.accdb";
+        int? currentId = null;
+
+        public EditTeamForm()
+        {
+            InitializeComponent();
+        }
+
+        public EditTeamForm(DataGridViewRow row)
+        {
+            InitializeComponent();
+            currentId = Convert.ToInt32(row.Cells["team_id"].Value);
+            textBox1.Text = row.Cells["team_name"].Value.ToString();
+            textBox2.Text = row.Cells["region"].Value.ToString();
+            textBox3.Text = row.Cells["tier_level"].Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                string query;
+                OleDbCommand cmd = new OleDbCommand();
+
+                if (currentId == null)
+                {
+                    query = "INSERT INTO Teams (team_name, region, tier_level) VALUES (?, ?, ?)";
+                }
+                else
+                {
+                    query = "UPDATE Teams SET team_name = ?, region = ?, tier_level = ? WHERE team_id = ?";
+                }
+
+                cmd.CommandText = query;
+                cmd.Connection = connection;
+
+                cmd.Parameters.AddWithValue("?", textBox1.Text);
+                cmd.Parameters.AddWithValue("?", textBox2.Text);
+                cmd.Parameters.AddWithValue("?", textBox3.Text);
+
+                if (currentId != null)
+                {
+                    cmd.Parameters.AddWithValue("?", currentId);
+                }
+
+                cmd.ExecuteNonQuery();
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
-using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+
 
 namespace WindowsFormsApp1
 {
@@ -17,28 +24,44 @@ namespace WindowsFormsApp1
 
         private void LoadData(string filter = "")
         {
-            try
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
             {
-                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                string query = "SELECT * FROM Teams";
+                if (!string.IsNullOrEmpty(filter))
                 {
-                    string query = "SELECT * FROM Teams";
-                    if (!string.IsNullOrEmpty(filter))
-                    {
-                        query += $" WHERE team_name LIKE '%{filter}%' OR region LIKE '%{filter}%'";
-                    }
-                    query += " ORDER BY tier_level DESC, team_name ASC";
-
-                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
-                    DataTable dt = new DataTable();
-                    adapter.Fill(dt);
-                    dataGridView1.DataSource = dt;
+                    query += $" WHERE team_name LIKE '%{filter}%' OR region LIKE '%{filter}%'";
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ошибка: " + ex.Message);
+                query += " ORDER BY tier_level DESC, team_name ASC";
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+                DataTable dt = new DataTable();
+
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                if (dataGridView1.Columns.Contains("team_id"))
+                {
+                    dataGridView1.Columns["team_id"].Visible = false;
+                }
+
+                if (dataGridView1.Columns.Contains("logo_path"))
+                {
+                    dataGridView1.Columns["logo_path"].Visible = false;
+                }
+
+                if (dataGridView1.Columns.Contains("team_name"))
+                    dataGridView1.Columns["team_name"].HeaderText = "Назва команди";
+
+                if (dataGridView1.Columns.Contains("region"))
+                    dataGridView1.Columns["region"].HeaderText = "Регіон";
+
+                if (dataGridView1.Columns.Contains("tier_level"))
+                    dataGridView1.Columns["tier_level"].HeaderText = "Рівень (Tier)";
+
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             }
         }
+
 
         private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -47,6 +70,58 @@ namespace WindowsFormsApp1
                 var row = dataGridView1.Rows[e.RowIndex];
                 DetailsForm details = new DetailsForm(row);
                 details.ShowDialog();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EditTeamForm editForm = new EditTeamForm();
+            if (editForm.ShowDialog() == DialogResult.OK)
+            {
+                LoadData();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                EditTeamForm editForm = new EditTeamForm(dataGridView1.CurrentRow);
+                if (editForm.ShowDialog() == DialogResult.OK)
+                {
+                    LoadData();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var result = MessageBox.Show("Видалити команду та ВСІХ її гравців?", "Увага", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["team_id"].Value);
+                    using (OleDbConnection connection = new OleDbConnection(connectionString))
+                    {
+                        connection.Open();
+                        string deletePlayersQuery = "DELETE FROM Players WHERE team_id = ?";
+                        OleDbCommand cmdPlayers = new OleDbCommand(deletePlayersQuery, connection);
+                        cmdPlayers.Parameters.AddWithValue("?", id);
+                        cmdPlayers.ExecuteNonQuery();
+
+                        string deleteTeamQuery = "DELETE FROM Teams WHERE team_id = ?";
+                        OleDbCommand cmdTeam = new OleDbCommand(deleteTeamQuery, connection);
+                        cmdTeam.Parameters.AddWithValue("?", id);
+                        cmdTeam.ExecuteNonQuery();
+                    }
+                    LoadData();
+                }
             }
         }
     }
